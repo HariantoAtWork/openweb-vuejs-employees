60ad1edb60230aa95165031a603772e5c68264d5
Harianto van Insulinde <hariantoatwork@gmail.com>
2017-04-21 00:30:50 +0200

    Restructure files

---
+++ resources/vue/component/section/Employee.vue
@@ -0,0 +1,16 @@
+<template>
+	<section class="employee-view">
+		<header class="employees-item">
+			<div class="avatar">
+			    <img class="avatar-circle" :src="item.profileImage" alt="">
+			</div>
+			<div class="u-flex">
+			    <div class="employer-name">{{ item.name }}</div>
+			    <div class="employer-role">{{ item.role }}</div>
+			</div>
+			<div>
+			    <span class="fa fa-chevron-right"></span>
+			</div>
+		</header>
+	</section>
+</template>
